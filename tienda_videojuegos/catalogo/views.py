d70b25c95f5c70a721279905ff77020d96df64cb
Mario Celis <celioso1@hotmail.com>
2026-02-15 20:03:10 -0500

    Create catalog app and templates folder

--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def lista_juegos(request):
+    juegos = [
+        {'nombre': 'Dogo Racing', 'precio': 29.99, 'plataforma':'PC, PS5, Xbox Serie X'},
+        {'nombre': 'PLatform', 'precio': 14.99, 'plataforma':'PC, Switch'},
+        {'nombre': 'Urban Darkness', 'precio': 39.99, 'plataforma':'PC, PS5'},
+        {'nombre': 'Highspeeed', 'precio': 29.99, 'plataforma':'PC, Xbox Serie X'},
+        {'nombre': 'Night Mode', 'precio': 19.99, 'plataforma':'PC, pS4, Xbox One'},
+        {'nombre': 'The Grand Thief', 'precio': 59.99, 'plataforma':'PC, PS5, Xbox Serie X'},
+        {'nombre': 'Sunset Vibe', 'precio': 24.99, 'plataforma':'PC, Switch, Mobile'},
+        {'nombre': 'Dark Whispers', 'precio': 34.99, 'plataforma':'PC, PS5, Xbox Serie X'},
+        {'nombre': 'Space Zero', 'precio': 44.99, 'plataforma':'PC, PS5'},
+    ]
+
+    contexto_catalogo_juegos = {'lista_juegos': juegos}
+    
+    return render(request, 'catalogo/lista_juegos.html', contexto_catalogo_juegos)
+    
