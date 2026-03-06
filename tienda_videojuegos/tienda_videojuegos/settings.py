6e3c32cf78720288994b7c1400fc1032613551fb
Mario Celis <celioso1@hotmail.com>
2026-03-05 20:25:28 -0500

    Add emojis

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "home",
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
