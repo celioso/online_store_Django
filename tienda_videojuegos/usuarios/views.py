bb3b60830e33df611d55cae123e8d15c3295a8dc
Mario Celis <celioso1@hotmail.com>
2026-03-28 20:40:05 -0500

    add forms and views modules

--- tienda_videojuegos/usuarios/views.py
+++ tienda_videojuegos/usuarios/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import RegistroForm, LoginForm
+from django.contrib import messages
 
-# Create your views here.
+def registro_view(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            return redirect('home')
+    else:
+        form = ReqistroForm()
+    return render(request, 'usuarios/registro.html',
+                {'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            return redirect('home')
+        else:
+            print(form.errors):
+            messages.error(request, 'usuarios/login.html', {'form': form})
